6e501735b3072803f29b4514665c1fbb0627d090
ankitnathtiwari <ankitfounder@gmail.com>
2021-07-21 22:32:39 +0530

    changed update post

--- controller/post/post-update/index.js
+++ controller/post/post-update/index.js
@@ -2,6 +2,7 @@ const { upload } = require("../../../middleware/multer/index");
 const { post } = require("../../../models/post/index");
 const { updatePost } = require("../../../dao/index");
 const { postValidation } = require("../../../services/validation/index");
+const { resizedImage } = require("../../../services/image-processing/index");
 
 const postUpdate = (req, res) => {
   //get the post data and file from the request
@@ -12,14 +13,25 @@ const postUpdate = (req, res) => {
     post
       .findOne({ _id: req.body._id })
       .then((post) => {
-        updatePost(post, req);
-        post.save().then((post) =>
-          res.json({
-            post: post,
-            updateStatus: true,
-            message: "Updated SuccessFully",
+        console.log(post, "updated post pre");
+        resizedImage(req, res)
+          .then(() => {
+            console.log(post, "updated post");
+            updatePost(post, req);
+            post.save().then((post) =>
+              res.json({
+                post: post,
+                updateStatus: true,
+                message: "Updated SuccessFully",
+              })
+            );
           })
-        );
+          .catch((err) =>
+            res.json({
+              updateStatus: false,
+              message: "Some Error Occured",
+            })
+          );
       })
       .catch((err) =>
         res.json({
