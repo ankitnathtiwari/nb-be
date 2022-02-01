28c00253beb5111bfa30f30471cb034aadccfc9e
ankitfounder@gmail.com <ankitfounder@gmail.com>
2022-02-02 01:56:11 +0530

    bug fix

--- controller/public/readVideo.js
+++ controller/public/readVideo.js
@@ -6,137 +6,151 @@ const config = require("config");
 const { topicValidation } = require("../../services/validation/index");
 
 const publicVideoFeed = async (req, res) => {
-  if (topicValidation(req.query.top)) {
-    if (req.query.top === "singlepost") {
-      const videoPostList = await videoPost.find({ _id: req.query.id });
-      const reporterIdList = videoPostList.map((item) => item.user);
-      const reporterDetailList = await user.find({
-        _id: { $in: reporterIdList },
-      });
+  try {
+    if (topicValidation(req.query.top)) {
+      if (req.query.top === "singlepost") {
+        const videoPostList = await videoPost.find({ _id: req.query.id });
+        const reporterIdList = videoPostList.map((item) => item.user);
+        const reporterDetailList = await user.find({
+          _id: { $in: reporterIdList },
+        });
 
-      const videoListWithAuthor = videoPostList.map((item) => {
-        for (let i = 0; i < reporterDetailList.length; i++) {
-          const element = reporterDetailList[i];
-          if (String(element._id) === item.user) {
-            return { ...item._doc, authorName: element.name, url: config.url };
-          }
-        }
-      });
-
-      if (req.session.user) {
-        const followList = await follow.find({ follower: req.session.user.id });
-        const followedList = followList.map((item) => item.followed);
-
-        if (followedList.length && videoPostList) {
-          let videoPostWithFollow = videoListWithAuthor.map((item) => {
-            if (followedList.includes(item.user)) {
-              return { ...item, followStatus: true };
+        const videoListWithAuthor = videoPostList.map((item) => {
+          for (let i = 0; i < reporterDetailList.length; i++) {
+            const element = reporterDetailList[i];
+            if (String(element._id) === item.user) {
+              return {
+                ...item._doc,
+                authorName: element.name,
+                url: config.url,
+              };
             }
-            return item;
+          }
+        });
+
+        if (req.session.user) {
+          const followList = await follow.find({
+            follower: req.session.user.id,
           });
-          res.json({ status: true, videoPosts: videoPostWithFollow });
+          const followedList = followList.map((item) => item.followed);
+
+          if (followedList.length && videoPostList) {
+            let videoPostWithFollow = videoListWithAuthor.map((item) => {
+              if (followedList.includes(item.user)) {
+                return { ...item, followStatus: true };
+              }
+              return item;
+            });
+            res.json({ status: true, videoPosts: videoPostWithFollow });
+          } else {
+            res.json({ status: true, videoPosts: videoListWithAuthor });
+          }
         } else {
-          res.json({ status: true, videoPosts: videoListWithAuthor });
+          return res.json({ status: true, videoPosts: videoListWithAuthor });
         }
-      } else {
+
         return res.json({ status: true, videoPosts: videoListWithAuthor });
       }
 
-      return res.json({ status: true, videoPosts: videoListWithAuthor });
-    }
+      if (req.query.top === "allpost" || req.query.top.length === 0) {
+        const videoPostList = await videoPost
+          .find()
+          .sort({ pub_date: -1 })
+          .skip((req.query.page - 1) * 5)
+          .limit(5)
+          .exec();
+        const reporterIdList = videoPostList.map((item) => item.user);
+        const reporterDetailList = await user.find({
+          _id: { $in: reporterIdList },
+        });
 
-    if (req.query.top === "allpost" || req.query.top.length === 0) {
-      const videoPostList = await videoPost
-        .find()
-        .sort({ pub_date: -1 })
-        .skip((req.query.page - 1) * 5)
-        .limit(5)
-        .exec();
-      const reporterIdList = videoPostList.map((item) => item.user);
-      const reporterDetailList = await user.find({
-        _id: { $in: reporterIdList },
-      });
-
-      const videoListWithAuthor = videoPostList.map((item) => {
-        for (let i = 0; i < reporterDetailList.length; i++) {
-          const element = reporterDetailList[i];
-          if (String(element._id) === item.user) {
-            return {
-              ...item._doc,
-              authorName: element.name,
-              followers_count: element.followers_count,
-              url: config.url,
-            };
-          }
-        }
-      });
-
-      if (req.session.user) {
-        const followList = await follow.find({ follower: req.session.user.id });
-        const followedList = followList.map((item) => item.followed);
-
-        if (followedList.length && videoPostList) {
-          let videoPostWithFollow = videoListWithAuthor.map((item) => {
-            if (followedList.includes(item.user)) {
-              return { ...item, followStatus: true };
+        const videoListWithAuthor = videoPostList.map((item) => {
+          for (let i = 0; i < reporterDetailList.length; i++) {
+            const element = reporterDetailList[i];
+            if (String(element._id) === item.user) {
+              return {
+                ...item._doc,
+                authorName: element.name,
+                followers_count: element.followers_count,
+                url: config.url,
+              };
             }
-            return item;
+          }
+        });
+
+        if (req.session.user) {
+          const followList = await follow.find({
+            follower: req.session.user.id,
           });
-          res.json({ status: true, videoPosts: videoPostWithFollow });
+          const followedList = followList.map((item) => item.followed);
+
+          if (followedList.length && videoPostList) {
+            let videoPostWithFollow = videoListWithAuthor.map((item) => {
+              if (followedList.includes(item.user)) {
+                return { ...item, followStatus: true };
+              }
+              return item;
+            });
+            res.json({ status: true, videoPosts: videoPostWithFollow });
+          } else {
+            res.json({ status: true, videoPosts: videoListWithAuthor });
+          }
         } else {
           res.json({ status: true, videoPosts: videoListWithAuthor });
         }
       } else {
-        res.json({ status: true, videoPosts: videoListWithAuthor });
+        const videoPostList = await videoPost
+          .find({ topics: req.query.top })
+          .sort({ pub_date: -1 })
+          .skip((req.query.page - 1) * 5)
+          .limit(5)
+          .exec();
+        const reporterIdList = videoPostList.map((item) => item.user);
+        const reporterDetailList = await user.find({
+          _id: { $in: reporterIdList },
+        });
+
+        const videoListWithAuthor = videoPostList.map((item) => {
+          for (let i = 0; i < reporterDetailList.length; i++) {
+            const element = reporterDetailList[i];
+            //change here also
+            if (String(element._id) === item.user) {
+              return {
+                ...item._doc,
+                authorName: element.name,
+                followers_count: element.followers_count,
+                url: config.url,
+              };
+            }
+          }
+        });
+
+        if (req.session.user) {
+          const followList = await follow.find({
+            follower: req.session.user.id,
+          });
+          const followedList = followList.map((item) => item.followed);
+
+          if (followedList.length && videoPostList) {
+            let videoPostWithFollow = videoListWithAuthor.map((item) => {
+              if (followedList.includes(item.user)) {
+                return { ...item, followStatus: true };
+              }
+              return item;
+            });
+            res.json({ status: true, videoPosts: videoPostWithFollow });
+          } else {
+            res.json({ status: true, videoPosts: videoListWithAuthor });
+          }
+        } else {
+          res.json({ status: true, videoPosts: videoListWithAuthor });
+        }
       }
     } else {
-      const videoPostList = await videoPost
-        .find({ topics: req.query.top })
-        .sort({ pub_date: -1 })
-        .skip((req.query.page - 1) * 5)
-        .limit(5)
-        .exec();
-      const reporterIdList = videoPostList.map((item) => item.user);
-      const reporterDetailList = await user.find({
-        _id: { $in: reporterIdList },
-      });
-
-      const videoListWithAuthor = videoPostList.map((item) => {
-        for (let i = 0; i < reporterDetailList.length; i++) {
-          const element = reporterDetailList[i];
-          //change here also
-          if (String(element._id) === item.user) {
-            return {
-              ...item._doc,
-              authorName: element.name,
-              followers_count: element.followers_count,
-              url: config.url,
-            };
-          }
-        }
-      });
-
-      if (req.session.user) {
-        const followList = await follow.find({ follower: req.session.user.id });
-        const followedList = followList.map((item) => item.followed);
-
-        if (followedList.length && videoPostList) {
-          let videoPostWithFollow = videoListWithAuthor.map((item) => {
-            if (followedList.includes(item.user)) {
-              return { ...item, followStatus: true };
-            }
-            return item;
-          });
-          res.json({ status: true, videoPosts: videoPostWithFollow });
-        } else {
-          res.json({ status: true, videoPosts: videoListWithAuthor });
-        }
-      } else {
-        res.json({ status: true, videoPosts: videoListWithAuthor });
-      }
+      res.json({ status: false, message: "invalid request" });
     }
-  } else {
-    res.json({ status: false, message: "invalid request" });
+  } catch (err) {
+    res.json({ status: false, message: err.message });
   }
 };
 
